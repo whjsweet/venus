79b239c671f6f92403495c34e583b421768976f1
alex <reizero@live.com>
2019-07-26 17:17:32 +0800

    集成react-redux saga router等

--- src/models/index.js
+++ src/models/index.js
@@ -1,10 +1,39 @@
-const models = (state = { number: 1 }, action) => {
-    switch (action.type) {
-        case 'increment': {
-            state.number += 1
-            return { ...state }
-        };
-        default: return state;
-    }
+// const models = (state = { number: 1 }, action) => {
+//     switch (action.type) {
+//         case 'increment': {
+//             state.number += 1
+//             return { ...state }
+//         };
+//         default: return state;
+//     }
+// };
+// export default models;
+
+export default {
+    namespace: 'first',
+
+    state: {},
+
+    subscriptions: {
+        setup({ dispatch, history }) {
+            // eslint-disable-line
+            history.listen(location => {
+                console.log(1, location);
+            });
+        },
+    },
+
+    effects: {
+        *fetch({ payload }, { call, put }) {
+            // eslint-disable-line
+            yield put({ type: 'save' });
+        },
+    },
+
+    reducers: {
+        increment(state, action) {
+            state.number += 1;
+            return state;
+        },
+    },
 };
-export default models;
--- src/webpack.config.base.js
+++ src/webpack.config.base.js
@@ -85,19 +85,20 @@ module.exports = {
         }]
       },
     ]
-  },
-  optimization: {
-    splitChunks: {
-      chunks: "all",
-      minChunks: 1,
-      minSize: 0,
-      cacheGroups: { // cacheGroups对象，定义了需要被抽离的模块
-        framework: {
-          test: "dist", // test属性是比较关键的一个值，他可以是一个字符串，也可以是正则表达式，还可以是函数。如果定义的是字符串，会匹配入口模块名称，会从其他模块中把包含这个模块的抽离出来
-          name: "dist", // 抽离后生成的名字，和入口文件模块名称相同，这样抽离出来的新生成的framework模块会覆盖被抽离的framework模块
-          enforce: true
-        }
-      }
-    }
   }
+  // ,
+  // optimization: {
+  //   splitChunks: {
+  //     chunks: "all",
+  //     minChunks: 1,
+  //     minSize: 0,
+  //     cacheGroups: { // cacheGroups对象，定义了需要被抽离的模块
+  //       framework: {
+  //         test: "dist", // test属性是比较关键的一个值，他可以是一个字符串，也可以是正则表达式，还可以是函数。如果定义的是字符串，会匹配入口模块名称，会从其他模块中把包含这个模块的抽离出来
+  //         name: "dist", // 抽离后生成的名字，和入口文件模块名称相同，这样抽离出来的新生成的framework模块会覆盖被抽离的framework模块
+  //         enforce: true
+  //       }
+  //     }
+  //   }
+  // }
 };
